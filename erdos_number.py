49e886abae9ae49d81231b056d748bd3d5ef1ba7
momcilo <milosh.maric.90@gmail.com>
2023-08-02 15:57:11 +0200

    added Bidirectional Breadth First Search (BBFS)

--- erdos_number.py
+++ erdos_number.py
@@ -1,5 +1,6 @@
 from DataAccess import DataAccess
 from time import time
+from types import FunctionType
 
 
 
@@ -21,12 +22,13 @@ def main():
     >> -c Milos Radovanovic 
     and as a response you will get all authors that "Milos Radovanovic" have worked with. 
           
-    If you want to see "PATH TO ERDOS" of some author, using Breadth First Search (BFS), insert text like this:
-    >> -p Milos Radovanovic
-    and as a response you will get shortest path (or multiple, if there is more than one) to the legendary mathematician "Paul Erdös"!
-
-    If you want to see only one "PATH TO ERDOS" of some author, using Iterative Deepening Search(IDS), insert text like this:
-    >> -o Milos Radovanovic
+    If you want to see "PATH TO ERDÖS" of some author, you have three choises:
+    1. Using Breadth First Search (BFS):
+    >> -bfs Milos Radovanovic
+    2. Using Iterative Deepening Search(IDS):
+    >> -ids Milos Radovanovic
+    3. Using Bidirectional Breadth First Search (BBFS)
+    >> -bbfs Milos Radovanovic
     and as a response you will get shortest path (or multiple, if there is more than one) to the legendary mathematician "Paul Erdös"!
 
     For exit type:
@@ -58,8 +60,8 @@ def main():
             print(f"You have only 1 arguments, and 2 is needed.")
             continue
 
-        if tokens[0] != "-a" and tokens[0] != "-c" and tokens[0] != "-p" and tokens[0] != "-o":
-            print(f"First argument can be either '-a' or '-c' or '-p'. Yours is '{tokens[0]}'")
+        if tokens[0] != "-a" and tokens[0] != "-c" and tokens[0] != "-bfs" and tokens[0] != "-ids" and tokens[0] != "-bbfs":
+            print(f"First argument can be either '-a' or '-c' or '-bfs' or '-ids' or '-bbfs'. Your is '{tokens[0]}'")
             continue
 
         if tokens[0] == "-a":
@@ -90,53 +92,87 @@ def main():
             
             continue
 
-        if tokens[0] == "-p":
-            if not db.check_author(tokens[1]):
-                print("There is no author with that name...")
-                continue
-
-            start_time = time()
-            paths = find_paths_bfs(tokens[1])
-            print(f"Execution time: {time() - start_time}")
-            if len(paths) == 0:
-                print(f"There are no paths from {tokens[1]} to {erdos_name}")
-                continue
-
-            print(f"Erdös number of {tokens[1]} is {len(paths[0])-1}")
-            print("Paths:")
-            for i in range(len(paths)):
-                print(f"{i+1}."," - ".join(paths[i]))
-            
+        if tokens[0] == "-bfs":
+            search(tokens[1], find_paths_bfs)
             continue
             
-        if tokens[0] == "-o":
-            if not db.check_author(tokens[1]):
-                print("There is no author with that name...")
-                continue
+        if tokens[0] == "-ids":
+            search(tokens[1], find_paths_ids)
+            continue
 
-            start_time = time()
-            paths = find_paths_id(tokens[1])
-            print(f"Execution time: {time() - start_time}")
-            if len(paths) == 0:
-                print(f"There are no paths from {tokens[1]} to {erdos_name}")
-                continue
-
-            print(f"Erdös number of {tokens[1]} is {len(paths[0])-1}")
-            print("Paths:")
-            for i in range(len(paths)):
-                print(f"{i+1}."," - ".join(paths[i]))
-            
+        if tokens[0] == "-bbfs":
+            search(tokens[1], find_paths_bbfs)
             continue
         
-def find_paths_id(author: str):
-    pass
+
+def search(author: str, fun: FunctionType):
+    if not db.check_author(author):
+        print("There is no author with that name...")
+        return
+
+    start_time = time()
+    paths = fun(author)
+    print(f"Execution time: {time() - start_time}")
+    if len(paths) == 0:
+        print(f"There are no paths from {author} to {erdos_name}")
+        return
+
+    print(f"Erdös number of {author} is {len(paths[0])-1}")
+    print("Paths:")
+    for i in range(len(paths)):
+        print(f"{i+1}."," - ".join(paths[i]))
+
+
+def find_paths_ids(author: str):
+    i = 0
+    while True:
+        result = limited_dfs(author, i, [author])
+        if result == None:
+            return [[]]
+        if result != [[]]:
+            return result
+        i += 1
+
+
+def limited_dfs(author: str, i: int, visited):
+    if i == 0:
+        if author == erdos_name:
+            return [[erdos_name]]
+        else:
+            return [[]]
+        
+    paths = []
+    colleagues = db.find_colleagues(author)
+    to_visit = [c for c in colleagues if c not in visited]
+    if to_visit == []:
+        return None
+    else:
+        empty_list = False
+        full_list = False
+        for coll in to_visit:
+            subpaths = limited_dfs(coll, i-1, visited + [coll])
+            if subpaths == None:
+                continue
+            if subpaths == [[]]:
+                empty_list = True
+                continue
+            
+            paths.extend([[author] + sub for sub in subpaths])
+            full_list = True
+
+    if full_list:
+        return paths
+    
+    if empty_list:
+        return [[]]
+    
+    return None
 
 def find_paths_bfs(author: str):
     if author == erdos_name:
         return [[erdos_name]]
 
     # Mapa je strukture {<element>: {"level": <broj>, "parents": [<parent1>, ...]}}
-    global path_map
     path_map = {}
     path_map[author] = {"level": 0, "parents": []}
     queue = []
@@ -170,7 +206,7 @@ def find_paths_bfs(author: str):
 
         j += 1
 
-        if j >= len(queue[-2]):
+        if j >= len(queue[i]):
             j = 0
             i += 1
             queue.append([])
@@ -182,20 +218,94 @@ def find_paths_bfs(author: str):
         return []       
     
     
-    paths = dfs_paths(erdos_name, author)
+    paths = dfs_paths(erdos_name, author, path_map)
     path_map = {}
     return paths
 
 
-def dfs_paths(curr: str, author: str):
+def dfs_paths(curr: str, author: str, map: dict):
     if curr == author:
         return [[author]]
     
-    parents = path_map[curr]["parents"]
+    parents = map[curr]["parents"]
 
     paths = []
     for v in parents:
-        paths.extend([a + [curr] for a in dfs_paths(v, author)])
+        paths.extend([a + [curr] for a in dfs_paths(v, author, map)])
+    
+    return paths
+
+
+def find_paths_bbfs(author: str):
+    if author == erdos_name:
+        return [[erdos_name]]
+
+    # path_map_bbfs sadrzi dve mape: 0 - polazi od authora, 1 - polazi od erdosa
+    # Mapa je strukture {<element>: {"level": <broj>, "parents": [<parent1>, ...]}}
+    path_map_bbfs = ({},{})
+    path_map_bbfs[0][author] = {"level": 0, "parents": []}
+    path_map_bbfs[1][erdos_name] = {"level": 0, "parents": []}
+
+    # Isto vazi i za queue
+    queue = ([[author],[]],[[erdos_name],[]])
+
+    path_exists = False
+
+    k = 0
+    i = 0
+
+
+    while True:
+        for j in range(len(queue[k][i])):
+            colleagues = db.find_colleagues(queue[k][i][j])
+            if not path_exists:
+                for coll in colleagues:
+                    if not path_exists:
+                        if coll in path_map_bbfs[(k+1)%2]:
+                            path_map_bbfs[k][coll] = {"level": i+1, "parents": [queue[k][i][j]]}
+                            path_exists = True
+                            continue
+
+                        #Check if coll is in that level in queue or above (smaller index), making sure we don't go backward
+                        if coll not in path_map_bbfs[k]:
+                            queue[k][i+1].append(coll)
+                            path_map_bbfs[k][coll] = {"level": i+1, "parents": [queue[k][i][j]]}
+                        elif path_map_bbfs[k][coll]["level"] == i+1:
+                            path_map_bbfs[k][coll]["parents"].append(queue[k][i][j])
+                    else:
+                        if coll in path_map_bbfs[(k+1)%2]:
+                            if coll not in path_map_bbfs[k]:
+                                path_map_bbfs[k][coll] = {"level": i+1, "parents": [queue[k][i][j]]}
+                            else:
+                                path_map_bbfs[k][coll]["parents"].append(queue[k][i][j])
+            else:
+                for coll in colleagues:
+                    if coll in path_map_bbfs[(k+1)%2]:
+                            if coll not in path_map_bbfs[k]:
+                                path_map_bbfs[k][coll] = {"level": i+1, "parents": [queue[k][i][j]]}
+                            else:
+                                path_map_bbfs[k][coll]["parents"].append(queue[k][i][j])
+
+        if path_exists or len(queue[k][i+1]) == 0:
+            break
+
+        queue[k].append([])
+        i += (k+1) // 2
+        k = (k+1) % 2
+    
+    if not path_exists:
+        return [[]]
+        
+    paths = []
+    intersection = [key for key in path_map_bbfs[0].keys() & path_map_bbfs[1].keys()]
+    
+    for k in intersection:
+        paths_author = [path[:-1] for path in dfs_paths(k,author,path_map_bbfs[0])]
+        paths_erdos = [path[-2::-1] for path in dfs_paths(k, erdos_name, path_map_bbfs[1])]
+        
+        for pa in paths_author:
+            for pe in paths_erdos:
+                paths.append(pa + [k] + pe)
     
     return paths
 
